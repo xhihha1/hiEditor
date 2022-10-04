141886d50b86b716334b5fbd12d8581a0f4e5a93
ken.yek <ken.yek@advantech.com.tw>
2022-10-04 16:41:42 +0800

    drag dialog event

--- drawingTool/common/script/common.js
+++ drawingTool/common/script/common.js
@@ -2,4 +2,15 @@
     $('.commonDialogClose').on('click', function(){
         $(this).parents('.commonDialog').hide()
     })
+    $('.commonDialogTitle').on("drag", function(e) {
+        var x = e.pageX;
+        var y = e.pageY;
+        var el = $(this).parents('.commonDialog');
+        // el.css('position', 'absolute');
+        if (x > 0 || y > 0) {
+            el.css("left", x);
+            el.css("top", y);
+        }
+        // https://javascript.info/mouse-drag-and-drop
+    })
 })(window)
